7e7b8673fee6616541ff248cf4e05313a087d572
Rayyan Panja <113234549+RayyanPanja@users.noreply.github.com>
2022-10-29 20:13:12 +0530

    DUNNO

--- assets/php/Settings/alter.php
+++ assets/php/Settings/alter.php
@@ -16,25 +16,6 @@ while ($data = mysqli_fetch_assoc($FetchMainResult)) {
     $myamount = $data['Amount'];
     $email = $data['Email'];
 }
-
-$first = date('Y-m-01');
-$last = date('Y-m-30');
-$spent = 0;
-$earn = 0;
-
-$Fetch2 = "SELECT * FROM `transaction` WHERE `From_Acc` = $myacc AND `Date` BETWEEN '$first' and '$last';";
-$result = mysqli_query($con, $Fetch2);
-while ($data = mysqli_fetch_assoc($result)) {
-    $spent += $data['Amount'];
-}
-
-$Fetch3 = "SELECT * FROM `transaction` WHERE `To_Acc` = $myacc AND `Date` BETWEEN '$first' and '$last';";
-$result3 = mysqli_query($con, $Fetch3);
-while ($data = mysqli_fetch_assoc($result3)) {
-    $earn += $data['Amount'];
-}
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -57,126 +38,91 @@ while ($data = mysqli_fetch_assoc($result3)) {
 
 </head>
 
-<body id="Home">
-    <dialog id="logoutform">
-        <div class="login-form">
-            <h1>Are You Sure?? You Want to Log Out</h1>
-            <form action="../logout.php" method="post">
-                <div class="login-btn-set">
-                    <button type="reset" id="CloseLogout" class="btn submit">Cancle</button>
-                    <button type="submit" class="btn cancle">Logout</button>
-                </div>
-            </form>
-        </div>
-    </dialog>
+<nav class="side-nav">
+    <div class="nav-head">
+        <h1> <span class="fa fa-bank"></span><span>Web Wallet</span></h1>
+    </div>
+    <div class="link-set">
+        <a href="../../../index.php" class="side-link">
+            <span class=" icon fa fa-home"></span> <span class="link">Home</span>
+        </a>
+        <a href="../transfer/transfer.php" class="side-link">
+            <span class=" icon fa fa-money-bill-transfer"></span> <span class="link">Transfer</span>
+        </a>
+        <a href="../Balance/balance.php" class="side-link">
+            <span class=" icon fa fa-cash-register"></span> <span class="link">Balance</span>
+        </a>
+        <a href="../Loan/step1.php" class="side-link">
+            <span class=" icon fa fa-dollar"></span> <span class="link">Loan</span>
+        </a>
+        <a href="dashboard.php" class="side-link">
+            <span class=" icon fa fa-gear"></span> <span class="link">Settings</span>
+        </a>
+        <a href="alter.php" class="side-link active">
+            <span class=" icon fa fa-gear active"></span> <span class="link">Change Settings</span>
+        </a>
+    </div>
 
-    <nav class="side-nav">
-        <div class="nav-head">
-            <h1> <span class="fa fa-bank"></span><span>Web Wallet</span></h1>
+    <div class="user-detail">
+        <i class="fa fa-user"></i>
+        <div class="username">
+            <?php echo $Name; ?>
         </div>
-        <div class="link-set">
-            <a href="../../../index.php" class="side-link">
-                <span class=" icon fa fa-home"></span> <span class="link">Home</span>
-            </a>
-            <a href="../transfer/transfer.php" class="side-link">
-                <span class=" icon fa fa-money-bill-transfer"></span> <span class="link">Transfer</span>
-            </a>
-            <a href="../Balance/balance.php" class="side-link">
-                <span class=" icon fa fa-cash-register"></span> <span class="link">Balance</span>
-            </a>
-            <a href="../Loan/step1.php" class="side-link">
-                <span class=" icon fa fa-dollar"></span> <span class="link">Loan</span>
-            </a>
-            <a href="dashboard.php" class="side-link">
-                <span class=" icon fa fa-gear"></span> <span class="link">Settings</span>
-            </a>
-            <a href="alter.php" class="side-link active">
-                <span class=" icon fa fa-gear active"></span> <span class="link">Change Settings</span>
-            </a>
-        </div>
+    </div>
+</nav>
 
-        <div class="user-detail">
-            <i class="fa fa-user"></i>
-            <div class="username">
-                <?php echo $Name; ?>
+<dialog class="change-box" id="name-box">
+    <h1>Change Name</h1>
+    <form action="" method="post">
+        <div class="row">
+            <div class="col-lab">
+                <label for="name">Name</label>
+            </div>
+            <div class="col-inp">
+                <input type="text" name="name" id="name" placeholder="Name..." class="input">
+            </div>
+            <div class="box-btn-pos">
+                <button type="submit" class="box-btn">Set</button>
             </div>
         </div>
-    </nav>
-    <section>
-        <dialog class="change-box" id="name-box">
-            <h1>Change Name</h1>
-            <form action="" method="post">
-                <div class="row">
-                    <div class="col-lab">
-                        <label for="name">Name</label>
-                    </div>
-                    <div class="col-inp">
-                        <input type="text" name="name" id="name" placeholder="Name..." class="input">
-                    </div>
-                    <div class="box-btn-pos">
-                        <button type="submit" class="box-btn">Set</button>
-                    </div>
-                </div>
-            </form>
+    </form>
 
-        </dialog>
+</dialog>
+
+
+<main id="dashboard">
+    <section class="top-sec">
+        <div class="hello">
+            <h1>Change Settings</h1>
+            <p>Welcome Back!! <?php echo $new[0] . $new[1]; ?></p>
+        </div>
     </section>
 
-
-    <main id="dashboard">
-        <section class="top-sec">
-            <div class="hello">
-                <h1>Change Settings</h1>
-                <p>Welcome Back!! <?php echo $new[0] . $new[1]; ?></p>
+    <section class="panel">
+        <div class="grid-3">
+            <div class="btn-holder">
+                <button class="dope-btn" id="toggle-name-btn">Change Name</button>
             </div>
-        </section>
-
-        <section class="panel">
-            <div class="grid-3">
-                <div class="btn-holder">
-                    <button class="dope-btn" id="toggle-name-btn">Change Name</button>
-                </div>
-                <div class="btn-holder">
-                    <button class="dope-btn" id="toggle-email-btn">Change Email</button>
-                </div>
-                <div class="btn-holder">
-                    <button class="dope-btn" id="toggle-contact-btn">Change Contact</button>
-                </div>
-                <div class="btn-holder">
-                    <button class="dope-btn" id="toggle-contact-btn">Change Contact</button>
-                </div>
-
+            <div class="btn-holder">
+                <button class="dope-btn" id="toggle-email-btn">Change Email</button>
+            </div>
+            <div class="btn-holder">
+                <button class="dope-btn" id="toggle-contact-btn">Change Contact</button>
+            </div>
+            <div class="btn-holder">
+                <button class="dope-btn" id="toggle-contact-btn">Change Contact</button>
+            </div>
+            <div class="btn-holder">
+                <button class="dope-btn" id="toggle-contact-btn">Change Password</button>
             </div>
 
-        </section>
-    </main>
+        </div>
+
+    </section>
+</main>
 </body>
 
 <script src="../../js/main.js"></script>
-<script>
-    // Logout Form Popup
-    const LogoutBtn = document.querySelector('#logout-btn');
-    const LogoutBtn2 = document.querySelector('#side-logout-btn');
-    const LogoutForm = document.querySelector('#logoutform');
-    const LogoutCloseBtn = document.querySelector('#CloseLogout');
-
-    LogoutBtn.addEventListener('click', () => {
-        console.log("ASDA");
-        LogoutForm.showModal();
-    });
-    LogoutBtn2.addEventListener('click', () => {
-        console.log("ASDA");
-        LogoutForm.showModal();
-    });
-    LogoutCloseBtn.addEventListener('click', () => {
-        LogoutForm.close();
-    });
-
-    window.addEventListener('dblclick', () => {
-        SideNav.close();
-        LoginForm.close();
-    });
-</script>
-
+<script src="../../js/setting.js"></script>
 
 </html>
