4bda79036defd1d60044345efc99dd3b7540856b
Rayyan Panja <113234549+RayyanPanja@users.noreply.github.com>
2022-10-31 17:21:51 +0530

    Dialog Handler

---
+++ admin-panel/assets/js/dialog.js
@@ -0,0 +1,17 @@
+function DialogHandler(Opener,Closer,Box,Modal) {
+    let Open = document.getElementById(Opener);
+    let Close = document.getElementById(Closer);
+    let Dialog = document.getElementById(Box);
+
+    Open.addEventListener('click',()=>{
+        if(Modal === true){
+            Dialog.showModal();
+        }else{
+            Dialog.show();
+        }
+    });
+
+    Close.addEventListener('click',()=>{
+        Dialog.close();
+    });    
+}
