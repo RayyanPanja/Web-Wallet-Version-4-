321ee54761642c2e31b41532ed27e8ac812a78b8
Rayyan Panja <113234549+RayyanPanja@users.noreply.github.com>
2022-11-07 12:07:42 +0530

    Cool Animation

--- admin-panel/assets/js/home-animate.js
+++ admin-panel/assets/js/home-animate.js
@@ -1,8 +1,20 @@
 const title = document.querySelector('#title');
 const login = document.querySelector('#login');
+const loginClose = document.querySelector('#close-login');
 setTimeout(() => {
     title.style.top = 40 + "%";
-    setInterval(() => {
+    setTimeout(() => {
         login.style.left = 50 + "%";
-    }, 500);
+    },500);
 }, 1000);
+
+login.addEventListener('click', () => {
+    setTimeout(() => {
+        login.style.left = 110 + "%";
+    }, 100);
+})
+loginClose.addEventListener('click', () => {
+    setTimeout(() => {
+        login.style.left = 50 + "%";
+    }, 100);
+})
